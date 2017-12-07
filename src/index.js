9813d7e0d265a4cf9b5c0632bebc613a202867b4
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-12-07 18:07:08 -0500

    Click on first box to add an X

--- src/index.js
+++ src/index.js
@@ -2,25 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Tictactoe extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {space1: "", space2: "", space3: "", space4: "", space5: "", space6: "", space7: "", space8: "", space9: ""};
+    this.markBoard = this.markBoard.bind(this);
+  }
+
+  markBoard() {
+    const x = this.state.space1 === "" ? "X" : "";
+    this.setState({space1: x})
+  }
+
   render() {
 
     return <table>
-      <tr>
-        <td id='square-1'></td>
-        <td id='square-2'></td>
-        <td id='square-3'></td>
-      </tr>
-      <tr>
-        <td id='square-4'></td>
-        <td id='square-5'></td>
-        <td id='square-6'></td>
-      </tr>
-      <tr>
-        <td id='square-7'></td>
-        <td id='square-8'></td>
-        <td id='square-9'></td>
-      </tr>
+      <tbody>
+        <tr>
+          <td id='square-1' onClick={this.markBoard}>{this.state.space1}</td>
+          <td id='square-2' onClick={this.markBoard}>{this.state.space2}</td>
+          <td id='square-3' onClick={this.markBoard}>{this.state.space3}</td>
+        </tr>
+        <tr>
+          <td id='square-4' onClick={this.markBoard}>{this.state.space4}</td>
+          <td id='square-5' onClick={this.markBoard}>{this.state.space5}</td>
+          <td id='square-6' onClick={this.markBoard}>{this.state.space6}</td>
+        </tr>
+        <tr>
+          <td id='square-7' onClick={this.markBoard}>{this.state.space7}</td>
+          <td id='square-8' onClick={this.markBoard}>{this.state.space8}</td>
+          <td id='square-9' onClick={this.markBoard}>{this.state.space9}</td>
+        </tr>
+      </tbody>
     </table>
   }
 }
