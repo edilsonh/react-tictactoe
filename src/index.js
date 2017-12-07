3089732061552b8bd89af7045c6f4606f7a56d82
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-12-07 16:35:33 -0500

    Refactor code to modify class for each box on board

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class MarkBoard extends React.Component {
+  
+  render() {
+    let td = document.getElementsByTagName('td');
+    for (let i = 0; i < td.length; i++) {
+      td[i].setAttribute("class","square-" + (i + 1));
+    }
+    return <section>{this.td}</section>
+  }
+}
 
-
-ReactDOM.render(<p></p>, document.getElementById('root'));
+ReactDOM.render(<MarkBoard />, document.getElementById('root'));
